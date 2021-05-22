380b80cd5f29be89f72fb5e1b733c4d2d33166f3
Jale-Abdullabayli <abdullabey085@gmail.com>
2021-05-22 17:17:12 +0400

    second

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$('.language').click(function(){
+$('.language').click(function() {
     $('.langDropdown').toggleClass('open');
+})
+
+
+$('.bell').hover(function() {
+    $(this).addClass('showNotification');
 })
