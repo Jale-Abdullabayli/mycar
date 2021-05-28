4226f102c3efd30f62a93e4f756133e89d057e38
iMac <imac@iMacs-iMac.local>
2021-05-28 16:51:34 +0400

    lst

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-$('.language').click(function() {
-    $('.langDropdown').toggleClass('open');
+$('.language').hover(function () {
+    $(this).toggleClass('open');
 })
 
 
-$('.bell').hover(function() {
+$('.bell').hover(function () {
     $(this).addClass('showNotification');
 })
+
+
+
+
+$(window).scroll(function (event) {
+    var windowHeight = $(window).height();
+    var scroll = $(window).scrollTop();
+    var car = $('.callToAction').offset().top;
+    var start = car - windowHeight;
+    var carHeight = $('.callToAction').height();
+    if (scroll >= start + carHeight) {
+        var carLeft = -60;
+        var carBottom = -40;
+        var rotate = 350;
+        var step = (scroll - start - carHeight) * 0.4;
+        carLeft += step;
+        carBottom += step;
+        rotate -= step * 0.13;
+        if (carLeft < 108) {
+            $('.car1').css("left", `${carLeft}px`).css("bottom", `${carBottom}px`);
+            $('.car1').css('transform', `rotateZ(${rotate}deg)`);
+        }
+
+    }
+});
