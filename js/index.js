b35d842abc095169258882fbfeecc43bca613618
Jale-Abdullabayli <abdullabey085@gmail.com>
2021-05-29 17:46:15 +0400

    fghjj

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-$('.language').hover(function () {
+$('.language').hover(function() {
     $(this).toggleClass('open');
 })
 
 
-$('.bell').hover(function () {
+$('.bell').hover(function() {
     $(this).addClass('showNotification');
 })
 
 
 
 
-$(window).scroll(function (event) {
+$(window).scroll(function(event) {
     var windowHeight = $(window).height();
     var scroll = $(window).scrollTop();
     var car = $('.callToAction').offset().top;
@@ -18,13 +18,13 @@ $(window).scroll(function (event) {
     var carHeight = $('.callToAction').height();
     if (scroll >= start + carHeight) {
         var carLeft = -60;
-        var carBottom = -40;
+        var carBottom = -50;
         var rotate = 350;
         var step = (scroll - start - carHeight) * 0.4;
         carLeft += step;
         carBottom += step;
         rotate -= step * 0.13;
-        if (carLeft < 108) {
+        if (carLeft < 105) {
             $('.car1').css("left", `${carLeft}px`).css("bottom", `${carBottom}px`);
             $('.car1').css('transform', `rotateZ(${rotate}deg)`);
         }
